a144cced4a5dbfbddfb4495023c965cfc097286a
Antony Najarro <anajarrom@miumg.edu.gt>
2026-03-05 14:48:41 -0600

    Primer ejercicio

--- Main.java
+++ Main.java
@@ -1,5 +1,24 @@
 package repasoParcial;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+	static Scanner Sc = new Scanner(System.in);
+	
+	public static void main (String []args) {
+		sumaDivisibles4();
+	}
+		// Ejercicio1
+		public static void sumaDivisibles4() {
+			System.out.println("Ingrese un numero:");
+			 int numero = Sc.nextInt();
+			 int suma = 0;
+			 
+			 for (int i = 1; i<= numero; i++) {
+				 if ( i % 4 == 0) {
+					 suma +=i;
+				 }
+			 }
+			 System.out.println("La suma es:" + suma);
+		}
 }
