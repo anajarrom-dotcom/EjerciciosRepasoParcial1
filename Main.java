ebc6bacd37230b9ae59107deebd9af90d34f8191
Antony Najarro <anajarrom@miumg.edu.gt>
2026-03-05 18:01:27 -0600

    Tercer Ejercicio

--- Main.java
+++ Main.java
@@ -22,6 +22,7 @@ public class Main {
 			 }
 			 System.out.println("La suma es:" + suma);
 		}
+		 // SegundoEjercicio
 		static void numeroFactorial() {
 			
 			long factorial = 1;
@@ -33,4 +34,23 @@ public class Main {
 		}
 			System.out.println("El factorial del numero es:" + factorial);
 }
+		//TercerEjercicio
+		static void numerosPositivosNegativos() {
+			
+			int num;
+			int positivos = 0,negativos = 0,ceros = 0;
+			System.out.println("Ingrese la cantidad de numeros");
+			int n = Sc.nextInt();
+			
+			for(int i = 1; i <= n; i++) {
+				num = Sc.nextInt();
+				
+				if(num>0)positivos++;
+				else if(num<0)negativos++;
+				else ceros++;
+			}
+			System.out.println("Numeros Positivos:" + positivos);
+			System.out.println("Numeros Negativos" + negativos);
+			System.out.println("Ceros:" + ceros);
+		}
 }
